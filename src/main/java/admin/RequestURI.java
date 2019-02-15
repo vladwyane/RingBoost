3cb013640e677e3516f48d095764a858106045b1
ellina.frolova <ellina.frolova@bigdropinc.com>
2019-02-15 18:11:14 +0200

    Change RoleUrl

--- src/main/java/admin/RequestURI.java
+++ src/main/java/admin/RequestURI.java
@@ -6,7 +6,7 @@ package admin;
 public class RequestURI {
 
     public static String SERVER_URI = "https://ringboost-dev.bigdropinc.net/admin/api/";
-    public static String LOGIN_URI = SERVER_URI + "login";
-    public static String ROLE_URI = SERVER_URI + "role";
-    public static String PERMISSION_URI = SERVER_URI + "permission";
+    public static String LOGIN_URI = SERVER_URI + "login/";
+    public static String ROLE_URI = SERVER_URI + "roles/";
+    public static String PERMISSION_URI = SERVER_URI + "permission/";
 }
